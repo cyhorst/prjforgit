c10bb8322d458d4807f5480d1a56fb47e86c9088
cyhorst <zhua@gmail.com>
2018-08-21 11:40:00 +0800

    新建工程测试

---
+++ src/com/cx/test/Hello.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, 长威有限公司
+ * FileName: Hello
+ * Author:   Administrator
+ * Date:     2018/8/21 11:34
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cx.test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2018/8/21
+ * @since 1.0.0
+ */
+public class Hello {
+}
